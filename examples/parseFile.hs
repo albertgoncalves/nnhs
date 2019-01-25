9960fb19163f5fe52751818332cc925f867931e4
albertgoncalves <goncalves.albert@gmail.com>
2019-01-24 19:31:43 -0500

    This is so sweet.

--- examples/parseFile.hs
+++ examples/parseFile.hs
@@ -6,14 +6,9 @@ import Text.Read (readMaybe)
 
 parseData :: String -> Maybe (Int, [Double])
 parseData x =
-    case x' of
-        (Just a:b) ->
-            case sequenceA b of
-                Just b' -> Just (floor a, b')
-                _ -> Nothing
+    case traverse readMaybe $ words x of
+        Just (a:b) -> Just (floor a, b)
         _ -> Nothing
-  where
-    x' = map readMaybe (words x) :: [Maybe Double]
 
 fmtData :: String -> [(Int, [Double])]
 fmtData = join . map (maybeToList . parseData) . lines
