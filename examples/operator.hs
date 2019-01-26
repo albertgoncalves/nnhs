df2d14808ba13bddaad5a5e6b1eda3a6fa41b7dc
albertgoncalves <goncalves.albert@gmail.com>
2019-01-26 02:02:40 -0500

    Cool stuff.

--- examples/operator.hs
+++ examples/operator.hs
@@ -3,7 +3,18 @@
 (|>) :: a -> (a -> b) -> b
 x |> f = f x
 
+(|.) :: (a -> b) -> (b -> c) -> a -> c
+f |. g = g . f -- alternatively: \x -> g (f x)
+
 main :: IO ()
 main = do
     let x = 10 :: Int
-    print $ x |> (+ 10) |> (: [1,2 ..]) |> drop 2 |> head
+    let f = (+ x)
+    let g = (: [x,x ..])
+    let h = take x
+    mapM_
+        print
+        [ x |> f |> g |> h |> sum --   |
+        , x |> (f |. g |. h |. sum) -- | equivalent expressions
+        , (sum . h . g . f) x --       |
+        ]
